ed763d9d88ac81512127929e8ecbe8588f49e6b0
satyamkumar420 <satyamkumar2460@gmail.com>
2024-05-19 10:46:03 +0530

    changes

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AI Recorder",
+  description:
+    "AI Recorder - Record your screen or any object's movement automatically with just one click. No more boring video editing.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main>{children}</main>
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/loding.tsx
@@ -0,0 +1,7 @@
+import { Rings } from "react-loader-spinner";
+
+const Loading = () => {
+  return <Rings color="red" />;
+};
+
+export default Loading;
